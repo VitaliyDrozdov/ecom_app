ef1356aa5f8ef4e8bc8272f86f3dc930d87e93c5
VitaliyDrozdov <vitdrzdv@mail.ru>
2025-02-01 14:36:35 +0300

    add alembic migrations

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
                                     create_async_engine)
@@ -5,10 +7,15 @@ from sqlalchemy.orm import DeclarativeBase
 
 load_dotenv()
 
-engine = create_async_engine(
-    "postgresql+asyncpg://ecommerce_user:password@localhost:5432/ecommerce_db",
-    echo=True,
-)
+
+class Config:
+    DB_USER = os.getenv("POSTGRES_USER")
+    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    DB_NAME = os.getenv("POSTGRES_DB")
+    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"  # noqa E501
+
+
+engine = create_async_engine(Config.DATABASE_URL, echo=True)
 async_sessionmaker_ = async_sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
 )
