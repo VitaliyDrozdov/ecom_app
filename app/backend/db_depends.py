ad973023a667d635e516496bb9cd3ad155321fee
VitaliyDrozdov <vitdrzdv@mail.ru>
2025-02-04 17:37:59 +0300

    add asnyc produtc routes

--- app/backend/db_depends.py
+++ app/backend/db_depends.py
@@ -1,9 +1,10 @@
-from app.backend.db import SessionLocal
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.backend.db import async_sessionmaker_
 
 
-async def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_sessionmaker_ as session:
+        yield session
--- app/routers/category.py
+++ app/routers/category.py
@@ -2,8 +2,8 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from slugify import slugify
-from sqlalchemy import insert, select, update
-from sqlalchemy.orm import Session
+from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.backend.db_depends import get_db
 from app.models import Category
@@ -13,58 +13,46 @@ router = APIRouter(prefix="/categories", tags=["Category"])
 
 
 @router.get("/")
-async def get_all_categories(db: Annotated[Session, Depends(get_db)]):
-    return db.scalars(select(Category).where(Category.is_active == True)).all()
-
-
-# старая версия:
-# categories = db.query(Category).filter(Category.is_active == True).all()
+async def get_all_categories(db: Annotated[AsyncSession, Depends(get_db)]):
+    categories = await db.scalars(
+        select(Category).where(Category.is_active == True)
+    )
+    return categories.all()
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_category(
-    db: Annotated[Session, Depends(get_db)], create_category: CreateCategory
+    db: Annotated[AsyncSession, Depends(get_db)],
+    create_category: CreateCategory,
 ):
-    db.execute(
+    await db.execute(
         insert(Category).values(
             name=create_category.name,
             parent_id=create_category.parent_id,
             slug=slugify(create_category.name),
         )
     )
-    db.commit()
-    # старый вариант:
-    #   category_model = Category(
-    #         name=create_category.name,
-    #         parent_id=create_category.parent_id,
-    #         slug=slugify(create_category.name)
-    #     )
-    #     db.add(category_model)
-    #     db.commit()
+    await db.commit()
     return {"status_code": status.HTTP_201_CREATED, "transaction": "Sucessful"}
 
 
 @router.put("/")
 async def update_category(
-    db: Annotated[Session, Depends(get_db)],
+    db: Annotated[AsyncSession, Depends(get_db)],
     category_id: int,
     update_category: CreateCategory,
 ):
-    category = db.scalar(select(Category).where(Category.id == category_id))
+    category = await db.scalar(
+        select(Category).where(Category.id == category_id)
+    )
     if category is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
         )
-    db.execute(
-        update(Category)
-        .where(Category.id == category_id)
-        .values(
-            name=update_category.name,
-            slug=slugify(update_category.name),
-            parent_id=update_category.parent_id,
-        )
-    )
-    db.commit()
+    category.name = update_category.name
+    category.slug = slugify(update_category.name)
+    category.parent_id = update_category.parent_id
+    await db.commit()
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Category update succeded",
@@ -73,19 +61,17 @@ async def update_category(
 
 @router.delete("/")
 async def delete_category(
-    db: Annotated[Session, Depends(get_db)], category_id: int
+    db: Annotated[AsyncSession, Depends(get_db)], category_id: int
 ):
-    category = db.scalar(select(Category).where(Category.id == category_id))
+    category = await db.scalar(
+        select(Category).where(Category.id == category_id)
+    )
     if category is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
         )
-    db.execute(
-        update(Category)
-        .where(Category.id == category_id)
-        .values(is_active=False)
-    )
-    db.commit()
+    category.is_active = False
+    await db.commit()
     return {
         "status_code": status.HTTP_200_OK,
         "transaction": "Category deleted",
