0ffe034c579e54f93cd9c557316cb72be362c021
VitaliyDrozdov <vitdrzdv@mail.ru>
2025-02-05 17:43:33 +0300

    add jwt auth

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import category, products
+from app.routers import auth, category, products
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ async def welcome() -> dict:
 
 app.include_router(category.router)
 app.include_router(products.router)
+app.include_router(auth.router)
